423da714145a901859773bb4f40ab39c33561575
Foo-x <www.sand.x@gmail.com>
2021-08-11 22:07:06 +0900

    feat: add youtube video component

---
+++ src/components/youtube-video.tsx
@@ -0,0 +1,20 @@
+import * as React from "react"
+import * as styles from "styles/components/youtube-video.module.css"
+
+type Props = {
+  videoId: string
+}
+
+const YouTubeVideo: React.FC<Props> = ({ videoId }) => {
+  return (
+    <div className={styles.container}>
+      <iframe
+        className={styles.video}
+        src={`https://www.youtube.com/embed/${videoId}`}
+        allowFullScreen
+      ></iframe>
+    </div>
+  )
+}
+
+export default YouTubeVideo

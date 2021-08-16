ee527a9f7c9c906df225bb0c87696f97f4047e28
Foo-x <www.sand.x@gmail.com>
2021-08-16 17:12:21 +0900

    feat: remove license info

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -61,20 +61,6 @@ const AboutPage: React.FC<PageProps> = ({ location }) => {
         </a>
         ) です。
       </p>
-      <h2 className={styles.heading}>ライセンス</h2>
-      <div>
-        Icons made by{" "}
-        <a
-          href="https://www.flaticon.com/authors/pixel-perfect"
-          title="Pixel perfect"
-        >
-          Pixel perfect
-        </a>{" "}
-        from{" "}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </div>
     </Layout>
   )
 }
